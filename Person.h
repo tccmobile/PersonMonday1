15902d05b9f8f755027587dc9f65f880f1798a0a
Will Smith <drwillsmith@gmail.com>
2026-09-14 10:49:35 -0500

    Initial commit

---
+++ Person.h
@@ -0,0 +1,31 @@
+//
+// Created by Will Smith on 9/14/26.
+//
+
+#ifndef PERSONMONDAY1_PERSON_H
+#define PERSONMONDAY1_PERSON_H
+#include <__fwd/string.h>
+#include <string>
+using namespace std;
+
+class Person {
+    private:
+        string name;
+        int age;
+        double height;
+    public:
+        string getName() const;
+        int getAge() const;
+        double getHeight() const;
+        void setName(string name);
+        void setAge(int age);
+        void setHeight(double height);
+        Person();
+        Person(string name);
+        Person(string name, int age, double height);
+        void hasBirthday();
+
+};
+
+
+#endif //PERSONMONDAY1_PERSON_H
